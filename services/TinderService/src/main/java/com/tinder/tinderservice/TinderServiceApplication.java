0c8e3087cc88f806fb4613d3bfd2480b1ce18162
Roushan kumar <roushankumar7231246@gmail.com>
2025-07-16 00:35:54 +0530

    enviromet file and application properties added for data base properties

--- services/TinderService/src/main/java/com/tinder/tinderservice/TinderServiceApplication.java
+++ services/TinderService/src/main/java/com/tinder/tinderservice/TinderServiceApplication.java
@@ -1,5 +1,6 @@
 package com.tinder.tinderservice;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TinderServiceApplication {
 
 	public static void main(String[] args) {
+		initializeEnvironmentVariables();
 		SpringApplication.run(TinderServiceApplication.class, args);
+	}
+
+	private static void initializeEnvironmentVariables() {
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach((entry) -> System.setProperty(entry.getKey(), entry.getValue()));
 	}
 
 }
